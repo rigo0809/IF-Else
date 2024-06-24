a76c9a7f761b621e5d7bfe33dcb8d793d9babce4
rigo0809 <169068411+rigo0809@users.noreply.github.com>
2024-06-24 12:13:34 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    int numerador = 16;
+    int denominador = 4;
+    int resto = numerador % denominador;
+    System.out.println("O resto da divisão é:" + resto);
+    
+  }
+}
